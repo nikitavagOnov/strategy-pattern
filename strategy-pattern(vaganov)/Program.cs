78aceb2261f8b86eac7cab2b643324ecd97bc03d
DemonEyes12 <anemnonov2@gmail.com>
2022-01-22 13:48:23 +0300

    Добавьте файлы проекта.

---
+++ strategy-pattern(vaganov)/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClassLibrary;
+
+namespace strategy_pattern_vaganov_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MallardDuck mallardDuck = new MallardDuck();
+            RedheadDuck redheadDuck = new RedheadDuck();
+
+            Duck[] ducks = { mallardDuck, redheadDuck};
+            foreach (Duck d in ducks)
+            {
+                Console.WriteLine(d.Display());
+                Console.WriteLine(d.Swim());
+                Console.WriteLine(d.Quack());
+            }
+            Console.ReadKey();
+        }
+    }
+}
